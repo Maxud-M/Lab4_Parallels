496d2bc9b0f7c58ea733cc3089c07b19236d6f64
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 13:08:44 +0300

    Scripted auto-commit on change (2021-12-01 13:08:44) by gitwatch.sh

--- src/main/java/StoreActore.java
+++ src/main/java/StoreActore.java
@@ -8,7 +8,7 @@ public class StoreActore extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().match(StoreMessage.class, m -> {store.put(m.getKey(), )});
+        return ReceiveBuilder.create().match(StoreMessage.class, m -> {store.put(m.getKey(), m.getValue())});
         return null;
     }
 }
