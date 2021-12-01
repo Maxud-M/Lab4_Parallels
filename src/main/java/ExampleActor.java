05ecdfa9a39da4b728ba94ad73aafb69150e8800
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 14:53:16 +0300

    Scripted auto-commit on change (2021-12-01 14:53:16) by gitwatch.sh

--- src/main/java/ExampleActor.java
+++ src/main/java/ExampleActor.java
@@ -1,2 +1,16 @@
-public class ExampleActor {
+import akka.actor.AbstractActor;
+
+public class ExampleActor extends AbstractActor {
+    public static enum Msg {
+        GREET, DONE;
+    }
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals(Msg.GREET, m -> {
+                    System.out.println("Hello World!");
+                    sender().tell(Msg.DONE, self());
+                })
+                .build();
+    }
 }
