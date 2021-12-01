ddcb6b7f76805282c34f187fb596f97d75768df6
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 18:57:30 +0300

    Scripted auto-commit on change (2021-12-01 18:57:30) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import akka.actor.ActorSystem;
+import akka.routing.Router;
 import akka.routing.RouterActor;
 
 public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
-        RouterActor router = .actorOf()
+        Router
 
     }
 }
