23e99838db8a1b60365714455a2bf88dca9b9988
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 18:07:12 +0300

    Scripted auto-commit on change (2021-12-01 18:07:12) by gitwatch.sh

--- src/main/java/TestExecutor.java
+++ src/main/java/TestExecutor.java
@@ -1,12 +1,19 @@
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.util.ArrayList;
 
 public class TestExecutor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().match(Message.class, m -> {
-            
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            engine.eval(m.getJsScript());
+            Invocable invocable = (Invocable) engine;
+            return invocable.invokeFunction(m.getFunctionName(), m.getParams());
         }).build();
     }
 
@@ -15,6 +22,22 @@ public class TestExecutor extends AbstractActor {
         private String jsScript;
         private ArrayList<String> params;
         private String expectedResult;
+
+        public String getFunctionName() {
+            return functionName;
+        }
+
+        public String getJsScript() {
+            return jsScript;
+        }
+
+        public String getExpectedResult() {
+            return expectedResult;
+        }
+
+        public ArrayList<String> getParams() {
+            return params;
+        }
     }
 
 
