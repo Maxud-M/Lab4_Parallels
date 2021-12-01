a50e8e0d2dd5ecf92bac9b82de97b0b15128664c
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 13:06:45 +0300

    Scripted auto-commit on change (2021-12-01 13:06:45) by gitwatch.sh

--- src/main/java/StoreActore.java
+++ src/main/java/StoreActore.java
@@ -1,2 +1,12 @@
-public class StoreActore {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreActore extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
