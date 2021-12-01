3ead3e03789f70fbbb11359c007c08c8fa9f6290
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 18:13:20 +0300

    Scripted auto-commit on change (2021-12-01 18:13:20) by gitwatch.sh

--- src/main/java/TestExecutor.java
+++ src/main/java/TestExecutor.java
@@ -13,7 +13,7 @@ public class TestExecutor extends AbstractActor {
             ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
             engine.eval(m.getJsScript());
             Invocable invocable = (Invocable) engine;
-            invocable.invokeFunction(m.getFunctionName(), m.getParams()).toString();
+            Object result = invocable.invokeFunction(m.getFunctionName(), m.getParams()).toString();
         }).build();
     }
 
