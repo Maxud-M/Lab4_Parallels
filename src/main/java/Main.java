cc304a54cada63e8cf577d2e5040a4e5f1b19369
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 19:15:18 +0300

    Scripted auto-commit on change (2021-12-01 19:15:18) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
         ActorRef store = system.actorOf(Props.create(StoreActor.class, "storeActor"));
-        ArrayList<Routees> routees;
+        ArrayList<Routees> routees = new ArrayList<>();
         routees.add(new ActorRefRoutee(store));
         Router router = new Router(new RoundRobinPool(), routees);
         ActorRef router = system.actorOf((Props.empty().withRouter(new RoundRobinPool(5))));
