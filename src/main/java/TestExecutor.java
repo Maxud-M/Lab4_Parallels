88ba6bf0ae6c72b717645cb851c723316653359f
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 18:00:31 +0300

    Scripted auto-commit on change (2021-12-01 18:00:31) by gitwatch.sh

--- src/main/java/TestExecutor.java
+++ src/main/java/TestExecutor.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 public class TestExecutor {
     public class StoreMessage{
         String functionName;
-        String js
+        String jsScript;
+        ArrayList<String> params;
+        String expectedResult;
     }
+
+    public class Get
 }
