2d3f74dbfb947605a99806ade1cb8d2865e86ed7
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-01 19:11:46 +0300

    Scripted auto-commit on change (2021-12-01 19:11:46) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,16 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.routing.RoundRobinPool;
-import akka.routing.Routee;
-import akka.routing.Router;
-import akka.routing.RouterActor;
+import akka.routing.*;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
-        Router router = new Router(new RoundRobinPool())
+        ArrayList<Routees> routees;
+        routees.add()
+        Router router = new Router(new RoundRobinPool(), routees);
         ActorRef router = system.actorOf((Props.empty().withRouter(new RoundRobinPool(5))));
 
 
