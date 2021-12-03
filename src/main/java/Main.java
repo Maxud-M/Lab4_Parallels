0417dfc3bd40514b35792613b46338eb5465d80d
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-03 22:35:24 +0300

    Scripted auto-commit on change (2021-12-03 22:35:24) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,13 +17,13 @@ import java.util.concurrent.CompletionStage;
 public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("routes");
-        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class, "storeActor"));
+        ActorRef storeActor = system.actorOf(Props.create(StoreActor.class), "storeActor");
         final SupervisorStrategy strategy =
                 new OneForOneStrategy(
                         5,
                         Duration.ofMinutes(1),
                         Collections.<Class<? extends Throwable>>singletonList(Exception.class));
-        ActorRef router = system.actorOf(new RoundRobinPool(5).withSupervisorStrategy(strategy).props(Props.create(TestExecutor.class, "")));
+        ActorRef router = system.actorOf(new RoundRobinPool(5).withSupervisorStrategy(strategy).props(Props.create(TestExecutor.class), "router"));
         router.
 
 
