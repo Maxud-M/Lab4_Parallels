42b20c716b11039153f06bf247eb96be7d709219
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-03 23:12:15 +0300

    Scripted auto-commit on change (2021-12-03 23:12:15) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,8 +38,9 @@ public class Main {
                                 );
                             })
                     ),
-            )
-            }))
+                    post(entity())
+
+            );
         }
     }
 
