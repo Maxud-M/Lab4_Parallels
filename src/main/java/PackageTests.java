a572f4dd22b47c6c8acb51d8b472de362bd0004e
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-03 23:31:00 +0300

    Scripted auto-commit on change (2021-12-03 23:31:00) by gitwatch.sh

--- src/main/java/PackageTests.java
+++ src/main/java/PackageTests.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 public class PackageTests {
+
+    const static final Srting
+
     @JsonProperty("packageId")
     int packageId;
 
