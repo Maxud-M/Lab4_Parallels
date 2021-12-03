8ed941173b10c2bb1f03c36be7624f418ed86131
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 00:42:20 +0300

    Scripted auto-commit on change (2021-12-04 00:42:19) by gitwatch.sh

--- src/main/java/PackageTests.java
+++ src/main/java/PackageTests.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 
 public class PackageTests {
 
-    public static final String FUNCTION_NAME = "functionName";
+    /*public static final String FUNCTION_NAME = "functionName";
     public static final String JS_SCRIPT = "jsScript";
     public static final String TESTS = "tests";
     public static final String EXPECTED_RESULT = "expectedResult";
     public static final String PARAMS = "params";
-    public static final String TEST_NAME = "testName";
+    public static final String TEST_NAME = "testName";*/
 
     //@JsonProperty(Constants.PACKAGE_ID)
     private int packageId;
